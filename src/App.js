8e4efde02c2a60597df36689b056eb17816a31a6
NurZorlu <110225274+NurZorlu@users.noreply.github.com>
2022-12-07 15:58:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Footer from './components/footer/footer';
+import List from './components/list';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Footer />
+      <List />
+    </div>
+  );
+}
+
+export default App;
